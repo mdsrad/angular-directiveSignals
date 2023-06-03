be853a0f1d226aaefe52174699544b8b0274b60a
mdsrad <mdsrad@gmail.com>
2023-06-04 01:17:54 +0200

    fin de signals

---
+++ src/app/signals/services/users-service.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable, map, tap } from 'rxjs';
+import { SingleUserResponse, User } from '../interfaces/user-request.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersServiceService {
+
+  private http = inject( HttpClient);
+  private baseurl = 'https://reqres.in/api/users'
+
+  getUserById( id: number): Observable<User>{
+    return this.http.get<SingleUserResponse>(`${ this.baseurl }/${ id }`)
+      .pipe(
+        map( response => response.data),
+        tap( console.log )
+      )
+  }
+
+}
